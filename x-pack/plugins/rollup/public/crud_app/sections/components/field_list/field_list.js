dede7155b0dc6df6b8ee9e6154328dd58969e93f
Bill McConaghy <bill.mcconaghy@elastic.co>
2018-10-19 13:49:50 -0400

    retain page when the user deletes a term in rollup job wizard

--- x-pack/plugins/rollup/public/crud_app/sections/components/field_list/field_list.js
+++ x-pack/plugins/rollup/public/crud_app/sections/components/field_list/field_list.js
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 import {
@@ -12,75 +12,95 @@ import {
   EuiEmptyPrompt,
 } from '@elastic/eui';
 
-export const FieldList = ({
-  columns,
-  fields,
-  onRemoveField,
-  addButton,
-  emptyMessage,
-}) => {
-  let message;
+export class FieldList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pageIndex: null
+    };
+  }
+  onTableChange = ({ page: { index: pageIndex } }) => {
+    this.setState({ pageIndex });
+  }
+  componentDidUpdate() {
+    const { pageIndex } = this.state;
+    if (this.table && pageIndex) {
+      this.table.setState({ pageIndex });
+    }
+  }
+  render() {
+    const {
+      columns,
+      fields,
+      onRemoveField,
+      addButton,
+      emptyMessage,
+    } = this.props;
+    let message;
 
-  if (!fields.length) {
-    message = (
-      <EuiEmptyPrompt
-        title={emptyMessage}
-        titleSize="xs"
-      />
+    if (!fields.length) {
+      message = (
+        <EuiEmptyPrompt
+          title={emptyMessage}
+          titleSize="xs"
+        />
+      );
+    }
+
+    let extendedColumns;
+
+    if (onRemoveField) {
+      extendedColumns = columns.concat({
+        name: 'Remove',
+        width: '80px',
+        actions: [{
+          name: 'Remove',
+          isPrimary: true,
+          description: 'Remove this field',
+          icon: 'trash',
+          type: 'icon',
+          color: 'danger',
+          onClick: (field) => onRemoveField(field),
+        }]
+      });
+    } else {
+      extendedColumns = columns;
+    }
+
+    const search = {
+      toolsRight: addButton ? addButton : undefined,
+      box: {
+        incremental: true,
+      },
+    };
+
+    const pagination = {
+      initialPageSize: 200,
+      pageSizeOptions: [20, 100, 200]
+    };
+
+    return (
+      <Fragment>
+        <EuiInMemoryTable
+          items={fields}
+          itemId="name"
+          columns={extendedColumns}
+          search={search}
+          pagination={pagination}
+          sorting={true}
+          message={message}
+          onTableChange={this.onTableChange}
+          ref={(table) => { this.table = table; }}
+        />
+      </Fragment>
     );
   }
 
-  let extendedColumns;
-
-  if (onRemoveField) {
-    extendedColumns = columns.concat({
-      name: 'Remove',
-      width: '80px',
-      actions: [{
-        name: 'Remove',
-        isPrimary: true,
-        description: 'Remove this field',
-        icon: 'trash',
-        type: 'icon',
-        color: 'danger',
-        onClick: (field) => onRemoveField(field),
-      }]
-    });
-  } else {
-    extendedColumns = columns;
+  static propTypes = {
+    columns: PropTypes.array.isRequired,
+    fields: PropTypes.array.isRequired,
+    onRemoveField: PropTypes.func,
+    addButton: PropTypes.node,
+    emptyMessage: PropTypes.node,
   }
-
-  const search = {
-    toolsRight: addButton ? addButton : undefined,
-    box: {
-      incremental: true,
-    },
-  };
-
-  const pagination = {
-    initialPageSize: 200,
-    pageSizeOptions: [20, 100, 200]
-  };
-
-  return (
-    <Fragment>
-      <EuiInMemoryTable
-        items={fields}
-        itemId="name"
-        columns={extendedColumns}
-        search={search}
-        pagination={pagination}
-        sorting={true}
-        message={message}
-      />
-    </Fragment>
-  );
-};
-
-FieldList.propTypes = {
-  columns: PropTypes.array.isRequired,
-  fields: PropTypes.array.isRequired,
-  onRemoveField: PropTypes.func,
-  addButton: PropTypes.node,
-  emptyMessage: PropTypes.node,
-};
+}
